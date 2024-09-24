c958cfe782cf0da230ada817052415a0e6e8cadf
Fahad Anwar <officerops5@gmail.com>
2024-09-24 12:33:25 +0500

    Create conditional.c
    Write a C program that reads two integers and checks whether they are multiplied or not.
    Test Data :
    Input the first number: 5
    Input the second number: 15
    Expected Output:
    Multiplied!

---
+++ conditional.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+
+int firstnumber,secondnumber,product;
+
+
+printf("Enter the first number:\n");
+scanf("%d",&firstnumber);
+printf("Enter the second number:\n");
+scanf("%d",&secondnumber);
+
+product = firstnumber*secondnumber;
+
+if (firstnumber * secondnumber)
+{
+    printf("multiplied\n");
+}
+else
+{
+    printf("Not multiplied");
+}
+
+return 0;
+}
